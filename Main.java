65eb9e35d0cc1de8f2bd459ef3c518f1f8c1c180
Grant Johnson <grantgtj@gmail.com>
2024-09-26 13:53:19 +0200

    Add Features

--- Main.java
+++ Main.java
@@ -1,17 +1,24 @@
 public class Main{
     
     private String greeting;
+    private String altgreeting;
 
     public Main (){
         greeting = "Hello World!";
+        altgreeting = "Hello Alternative World";
     }
 
     public void printGreeting(){
         System.out.println(greeting);
     }
+
+    public void printAltGreeting(){
+        System.out.println(altgreeting);
+    }
     public static void main(String[] args) {
         Main main = new Main();
         main.printGreeting();
+        main.printAltGreeting();
     }
 
 
